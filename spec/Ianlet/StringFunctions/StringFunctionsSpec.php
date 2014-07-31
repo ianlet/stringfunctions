27d9498b1aa329a964c6c7595ece1af63ef62aab
Ian Létourneau <ian.letourneau.1@ulaval.ca>
2014-07-31 07:40:29 -0400

    Added basic slug and snake case functions

--- spec/Ianlet/StringFunctions/StringFunctionsSpec.php
+++ spec/Ianlet/StringFunctions/StringFunctionsSpec.php
@@ -11,4 +11,20 @@ class StringFunctionsSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Ianlet\StringFunctions\StringFunctions');
     }
+
+    function it_formats_a_string_to_camel_case_format()
+    {
+        $this::snakeCase('This, is a (great) string!')->shouldReturn('this_is_a_great_string');
+        $this::snakeCase('-Maybe this OneIs even-better!')->shouldReturn('maybe_this_oneis_even_better');
+        $this::snakeCase('-What happens---with---multiple-?!')->shouldReturn('what_happens_with_multiple');
+        $this::snakeCase('Nín hǎo. Wǒ shì zhōng guó rén')->shouldReturn('nin_hao_wo_shi_zhong_guo_ren');
+    }
+
+    function it_formats_a_string_to_a_slug()
+    {
+        $this::slug('A great organization name')->shouldReturn('a-great-organization-name');
+        $this::slug('An ltd. organization')->shouldReturn('an-ltd-organization');
+        $this::slug('Uncommon_organization_name')->shouldReturn('uncommon-organization-name');
+        $this::slug('Nín hǎo. Wǒ shì zhōng guó rén')->shouldReturn('nin-hao-wo-shi-zhong-guo-ren');
+    }
 }
--- src/Ianlet/StringFunctions/StringFunctions.php
+++ src/Ianlet/StringFunctions/StringFunctions.php
@@ -10,4 +10,38 @@ namespace Ianlet\StringFunctions;
  */
 class StringFunctions
 {
+    /**
+     * Format a string to a snake case string
+     * @param $string
+     * @return string
+     */
+    static public function snakeCase($string)
+    {
+        $string = self::slug($string, '_');
+
+        return $string;
+    }
+
+    /**
+     * Generate the slug of a string with a given delimiter (default delimiter is '-')
+     * @param string $string
+     * @param string $delimiter
+     * @return string
+     */
+    static public function slug($string, $delimiter = '-')
+    {
+        $string = htmlspecialchars_decode($string, ENT_QUOTES);
+
+        $string = preg_replace('~[^\\pL\d]+~u', $delimiter, $string);
+
+        $string = trim($string, $delimiter);
+
+        $string = iconv('utf-8', 'ASCII//TRANSLIT', $string);
+
+        $string = strtolower($string);
+
+        $string = preg_replace('~[^-\w]+~', '', $string);
+
+        return $string;
+    }
 }
